d84555cf570a12a514a4bbcb87313a44b780b452
Jon <64451392+all-inn-io@users.noreply.github.com>
2025-06-03 15:00:34 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,8 @@ import { componentTagger } from "lovable-tagger";
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
-    port: 8080,
+    // commented out for namecheap deployment
+    // port: 8080,
   },
   plugins: [
     react(),
